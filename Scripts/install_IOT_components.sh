6978e493b9f67dfa8a2059d7b3fa7cd666e1094a
Richard Guthrie <richardguthrie@Richards-MacBook-Pro.local>
2017-12-04 15:11:53 -0800

    Adding installation script for install pre-reqs on device VM.

---
+++ Scripts/install_IOT_components.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+CONN_STR="$1"
+
+echo 'Recieved connection string '$CONN_STR
+
+sudo apt-get update
+
+#Install docker
+echo 'Downloading docker'
+curl -fsSL get.docker.com -o get-docker.sh
+
+echo 'Installing docker'
+sudo sh get-docker.sh
+
+#install python 2.7 pip
+sudo apt-get install python-pip -y
+
+#install IOT Edge runtime
+sudo pip install -U azure-iot-edge-runtime-ctl
+
+if [ ! -z $CONN_STR ]; then
+	echo 'Attempting to setup device with connection string {'$CONN_STR'}'
+	sudo iotedgectl setup --connection-string $CONN_STR --auto-cert-gen-force-no-passwords
+
+	echo 'starting up device.'
+	sudo iotedgectl start
+else
+	echo 'No connection string set, exiting.'
+fi
